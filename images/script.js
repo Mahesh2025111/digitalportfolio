a167a4ab1a50790345bf176a82227500d061b4cc
Mahesh2025111 <mksvadivu@gmail.com>
2025-01-02 00:14:56 -0800

    Add files via upload

---
+++ images/script.js
@@ -0,0 +1,2 @@
+var name = "shanmugavadivu"
+console.log(name);
---
+++ script.js
@@ -0,0 +1,4 @@
+var first_name = "mahesh";
+let last_name = "shanmugavadivu";
+const dob = "01/02/1969"
+console.log(first_name,last_name,dob);
